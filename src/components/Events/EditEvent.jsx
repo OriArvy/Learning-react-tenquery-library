6aa1e9f8d796012c929557b127fba9f151e1549e
Rokas <rokas.vaitkevicius1@gmail.com>
2024-02-04 10:38:32 +0200

    Added updating functionality with optimistic updating

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,20 +1,77 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { fetchEvent, queryClient, updateEvent } from '../util/http.js';
+import LoadingIndicator from '../UI/LoadingIndicator.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const params = useParams()
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['events', params.id],
+    queryFn: (signal) => fetchEvent({ signal, id: params.id })
+  })
 
-  function handleSubmit(formData) {}
+  const { mutate } = useMutation({
+    mutationFn: updateEvent,
+    onMutate: async (data) => { // optimistic update approach updates before quering the DB
+      const newEvent = data.event // Data comes from mutate in handleSubmit formData in this case
+
+      await queryClient.cancelQueries({ queryKey: ['events', params.id]})
+      const previousEvent = queryClient.getQueryData(['events', params.id]);
+
+      queryClient.setQueryData(['events', params.id], newEvent)
+      
+      return { previousEvent: previousEvent }
+    },
+    onError: (error, data, context) => {
+      queryClient.setQueryData(['events', params.id], context.previousEvent) // rolling back if update fails
+    },
+    onSettled: () => { // syncs frontend+backend back up in case it was different 
+      queryClient.invalidateQueries(['events', params.id])
+    }
+    // onSuccess: () => {
+    //   queryClient.invalidateQueries({queryKey: ['events']})
+    //   navigate('../')
+    // }
+  })
+
+  function handleSubmit(formData) {
+    mutate({ id: params.id, event: formData})
+    navigate('../')
+  }
 
   function handleClose() {
     navigate('../');
   }
 
-  return (
-    <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
+  let content
+
+  if (isPending) {
+    content =
+    <div className='center'>
+      <LoadingIndicator />
+    </div>
+  }
+
+  if (isError) {
+    content =
+    <>
+      <ErrorBlock title='Failed to load event' message={error.info?.message || 'Failed to load event, check inputs'} />
+      <div className='form-actions'>
+        <Link to='../' className='button'>
+          Okay
+        </Link>
+      </div>
+    </>
+  }
+
+  if (data) {
+    content = (
+      <EventForm inputData={data} onSubmit={handleSubmit}>
         <Link to="../" className="button-text">
           Cancel
         </Link>
@@ -22,6 +79,12 @@ export default function EditEvent() {
           Update
         </button>
       </EventForm>
+    )
+  }
+
+  return (
+    <Modal onClose={handleClose}>
+      {content}
     </Modal>
   );
 }
